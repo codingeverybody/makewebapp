59fd1fffb3841f0e5d12b198ade8cb87c39d7e2a
egoing <egoing@gmail.com>
2015-04-30 10:16:04 +0900

    변수 https://opentutorials.org/module/1514/9369

---
+++ phpjs/4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="utf-8">
+</head>
+<body>
+  <h1>javascript</h1>
+  <script charset="utf-8">
+    name = "egoing";
+    document.write("안녕하세요."+name);
+  </script>
+  <h1>php</h1>
+  <?php
+    $name = "egoing";
+    echo "안녕하세요.".$name;
+  ?>
+</body>
+</html>
