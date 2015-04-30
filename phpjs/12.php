b1567b04df929fa6229ff61fa875d89624d4d9de
egoing <egoing@gmail.com>
2015-04-30 10:22:51 +0900

    함수 https://opentutorials.org/module/1514/9377

---
+++ phpjs/12.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="utf-8">
+</head>
+<body>
+  <h1>JavaScript</h1>
+  <script>
+    function a(){
+      document.write("Hello JS Function");
+    }
+    a();
+  </script>
+
+  <h1>php</h1>
+  <?php
+    function a(){
+      echo "Hello PHP Function ";
+    }
+    a();
+  ?>
+</body>
+</html>
---
+++ phpjs/13.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="utf-8">
+</head>
+<body>
+  <h1>JavaScript</h1>
+  <script>
+    function a(input){
+      return input+1;
+    }
+    document.write(a(6));
+  </script>
+
+  <h1>php</h1>
+  <?php
+    function a($input){
+      return $input+1;
+    }
+    echo a(6);
+  ?>
+</body>
+</html>
