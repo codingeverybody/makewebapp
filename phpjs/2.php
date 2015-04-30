bc2a832f5e19a56c6d1b1c5c8dccf95b2227ec38
egoing <egoing@gmail.com>
2015-04-30 10:13:03 +0900

    데이터타입과 연산자 https://opentutorials.org/module/1514/9367

---
+++ phpjs/2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="utf-8">
+</head>
+<body>
+  <h1>javascript</h1>
+  <script>
+    document.write("10"+"10");
+  </script>
+  <h2>php</h2>
+  <?php
+    echo "10"."10";
+  ?>
+</body>
+</html>
