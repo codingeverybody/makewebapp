62ca8e852feb653e500818e607095d4214a4d3db
egoing <egoing@gmail.com>
2015-04-30 10:14:29 +0900

    디버깅 https://opentutorials.org/module/1514/9368

---
+++ phpjs/3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="utf-8">
+</head>
+<body>
+  <script charset="utf-8">
+    document.write("hello world");
+  </script>
+  <?php
+
+  ?>
+</body>
+</html>
