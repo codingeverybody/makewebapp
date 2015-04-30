29b55506a01907e7e0f177a6ef905216cb0534f6
egoing <egoing@gmail.com>
2015-04-30 10:16:58 +0900

    비교 https://opentutorials.org/module/1514/9370

---
+++ phpjs/5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="utf-8">
+</head>
+<body>
+  <h1>JavaScript</h1>
+  <script charset="utf-8">
+    document.write(2==1);
+  </script>
+  <h1>php</h1>
+  <?php
+    var_dump(2==1);
+  ?>
+</body>
+</html>
