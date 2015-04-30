045ca88401621d87b09d666d5a918d80a00b3edf
egoing <egoing@gmail.com>
2015-04-30 10:20:06 +0900

    반복문 https://opentutorials.org/module/1514/9374

---
+++ phpjs/9.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="utf-8">
+</head>
+<body>
+  <h1>JavaScript</h1>
+  <ul>
+  <script>
+    i = 0;
+    while(i < 10){
+      document.write("<li>hello world</li>");
+      i = i + 1;
+    }
+  </script>
+  </ul>
+
+  <h2>php</h2>
+  <ul>
+  <?php
+    $i = 0;
+    while($i < 10){
+      echo "<li>hello world</li>";
+      $i = $i + 1;
+    }
+  ?>
+  </ul>
+</body>
+</html>
