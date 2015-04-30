10acb95ccd8fe5c4bea6eabd9c7e02dfc7b9a03a
egoing <egoing@gmail.com>
2015-04-30 10:11:55 +0900

    웹페이지에 코드 삽입하기 https://opentutorials.org/module/1514/9366

---
+++ phpjs/1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="utf-8">
+</head>
+<body>
+  <h1>php</h1>
+  <?php
+    echo 10+10;
+  ?>
+  <h1>JavaScript</h1>
+  <script>
+    document.write(10+10);
+  </script>
+</body>
+</html>
