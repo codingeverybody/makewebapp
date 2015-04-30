ee7f4e31d8f61fa4f64324246500bf972ac99ab3
egoing <egoing@gmail.com>
2015-04-30 10:17:50 +0900

    조건문 https://opentutorials.org/module/1514/9371

---
+++ phpjs/6.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="utf-8">
+</head>
+<body>
+  <h1>JavaScript</h1>
+  <script>
+    result = (1==2);
+    if(result){
+      document.write("참");
+    } else {
+      document.write("거짓");
+    }
+  </script>
+  <h1>php</h1>
+  <?php
+    $result = (2==2);
+    if($result){
+      echo "참";
+    } else {
+      echo "거짓";
+    }
+  ?>
+</body>
+</html>
